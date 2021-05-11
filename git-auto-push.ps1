dfb0f33dac53bc3bef90a7e220a2e64cbee171bf
Michael <proios.michael@gmail.com>
2021-05-11 03:52:02 -0400

    Initial Commit

---
+++ git-auto-push.ps1
@@ -0,0 +1,16 @@
+﻿$Date = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
+
+Get-ChildItem -Path ‘C:\Users\Michael Proios\Desktop\Scripts’ -Depth 0 -Recurse -Directory |
+
+ForEach-Object {
+    
+    Write-Output "Pushing $_ to Bitbucket"
+  
+    CD $_.FullName  
+    
+    git add --all 2>$null
+    
+    git commit -m "System Automated Commit for $Date" 2>$null 
+
+    git push origin master --force 2>$null     
+}
